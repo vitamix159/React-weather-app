7ce5cc1907c9fc76898ea48bcfa76972ef62af01
vitamix159 <97109007+vitamix159@users.noreply.github.com>
2022-04-22 14:21:04 +0300

    Added link to my GitHub account

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
         <h1>Weather App</h1>
         <h3>Search Engine</h3>
         <Search />
+        <p>
+          <a href="https://github.com/vitamix159/SheCodesProject-Weather-app" target="_blank" rel="noreferrer">Open-source code</a> by Vitalija Ramonaitė
+        </p>
       </header>
     </div>
   );
